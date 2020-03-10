2d599e8be411f03a0ec73b0c527fa80b5c0e3011
LarsenKyle <krlars87@gmail.com>
2020-03-10 14:49:39 -0400

    Moblie Styling

--- src/Containers/RouteView/RouteView.js
+++ src/Containers/RouteView/RouteView.js
@@ -7,7 +7,8 @@ class RouteView extends Component {
   state = {
     route: null,
     loading: false,
-    starToggle: false
+    starToggle: false,
+    pictureToggle: false
   };
   async componentDidMount() {
     let isSaved = JSON.parse(localStorage.getItem("routeIDs"));
@@ -24,7 +25,12 @@ class RouteView extends Component {
       this.setState({ route: ref.data.routes[0] });
     }
   }
-
+  pictureToggleHandler = () => {
+    const opposite = !this.state.pictureToggle;
+    this.setState({
+      pictureToggle: opposite
+    });
+  };
   saveRoute = () => {
     if (!this.state.starToggle) {
       let routeIDs = JSON.parse(localStorage.getItem("routeIDs"));
@@ -36,8 +42,6 @@ class RouteView extends Component {
       let filteredRoute = routeIDs.filter(route => {
         return route != this.state.route.id;
       });
-      console.log(filteredRoute);
-
       localStorage.setItem("routeIDs", JSON.stringify(filteredRoute));
     }
     const opposite = !this.state.starToggle;
@@ -56,51 +60,57 @@ class RouteView extends Component {
       <div>
         {this.state.route ? (
           <div className="line">
-            <div className="flex">
-              <div className="info">
-                <h2>{this.state.route.name}</h2>
-                {!this.state.starToggle ? (
-                  <svg
-                    onClick={this.saveRoute}
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    width="24"
-                  >
-                    <path d="M0 0h24v24H0V0z" fill="none" />
-                    <path d="M19.65 9.04l-4.84-.42-1.89-4.45c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5 4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.73 3.67-3.18c.67-.58.32-1.68-.56-1.75zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z" />
-                  </svg>
-                ) : (
-                  <svg
-                    onClick={this.saveRoute}
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    width="24"
-                  >
-                    <path d="M0 0h24v24H0z" fill="none" />
-                    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-                    <path d="M0 0h24v24H0z" fill="none" />
-                  </svg>
-                )}
-                <p className="local">Location</p>
-                <div className="info-line">{location}</div>
-                <div className="info-line">
-                  <p className="emph">Type:</p> <p>{this.state.route.type}</p>
-                </div>
-                <div className="info-line">
-                  <p className="emph">Grade:</p>{" "}
-                  <p>{this.state.route.rating}</p>
-                </div>
-                <div className="info-line">
-                  <p className="emph">Rating:</p>{" "}
-                  <p> {this.state.route.stars} Stars</p>
-                </div>
+            <div className="info">
+              <h2>{this.state.route.name}</h2>
+              {!this.state.starToggle ? (
+                <svg
+                  onClick={this.saveRoute}
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M0 0h24v24H0V0z" fill="none" />
+                  <path d="M19.65 9.04l-4.84-.42-1.89-4.45c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5 4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.73 3.67-3.18c.67-.58.32-1.68-.56-1.75zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z" />
+                </svg>
+              ) : (
+                <svg
+                  onClick={this.saveRoute}
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M0 0h24v24H0z" fill="none" />
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                  <path d="M0 0h24v24H0z" fill="none" />
+                </svg>
+              )}
+              <p className="local">Location</p>
+              <div className="info-line">{location}</div>
+              <div className="info-line">
+                <p className="emph">Type:</p> <p>{this.state.route.type}</p>
               </div>
-              {this.state.route.imgSmallMed ? (
-                <img alt="Selected Route" src={this.state.route.imgSmallMed} />
-              ) : null}
+              <div className="info-line">
+                <p className="emph">Grade:</p> <p>{this.state.route.rating}</p>
+              </div>
+              <div className="info-line">
+                <p className="emph">Rating:</p>{" "}
+                <p> {this.state.route.stars} Stars</p>
+              </div>
+              <div className="info-line-button">
+                {this.state.route.imgSmallMed ? (
+                  <button onClick={this.pictureToggleHandler} className="btn">
+                    Show Picture
+                  </button>
+                ) : (
+                  <p className="btn">No Picture Available</p>
+                )}
+              </div>
             </div>
+            {this.state.pictureToggle ? (
+              <img alt="Selected Route" src={this.state.route.imgSmallMed} />
+            ) : null}
           </div>
         ) : (
           <p>loading...</p>
